cd2951ccf03c8b01756ce27a959549da8942cade
nxlaa <ristiantoalan1@gmail.com>
2025-10-07 12:07:53 +0700

    change host

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "127.0.0.1";
+const HOST = process.env.HOST || "0.0.0.0";
 
 // Middleware
 app.use(cors());
